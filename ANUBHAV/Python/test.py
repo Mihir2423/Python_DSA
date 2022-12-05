2c947fb0d659439656b716457fdb661bdbea2ea3
Anubhav6393 <mishraanubhav8126@gmail.com>
2022-12-05 11:36:12 +0530

    Test of Variable

---
+++ ANUBHAV/Python/test.py
@@ -0,0 +1,15 @@
+#name all the data type
+"""int,float,str,bool"""
+
+#Take all the data type input from user and print the, with proper message
+n= int(input("Enter a NO ="))
+print(f"result ={n}")
+
+n= float(input("Enter a NO ="))
+print(f"RESULT = {n}")
+
+word= str(input("Enter a Name= "))
+print(f"name = {word}")
+
+s= True
+print(f"{s}")
